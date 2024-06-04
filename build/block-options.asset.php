848bd4a2a1260d18bc450800a5979605f4c0c36a
William Fridh <william.fridh@gmail.com>
2024-06-04 16:53:57 +0200

    Added built version

---
+++ build/block-options.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-components', 'wp-compose', 'wp-hooks', 'wp-i18n'), 'version' => 'aeb91495658d12ededd9');
---
+++ build/pages.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wp-components', 'wp-element'), 'version' => '3b066f0890ada6d9883e');
